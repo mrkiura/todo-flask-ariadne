3e54ab94ddf5d96c2f46ca25f9adde8f9b4ee3bf
Alex Kiura <kiuraalex@gmail.com>
2020-10-21 01:22:14 +0300

    Persist todos in database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import uuid
+import os
+from datetime import datetime
 
 from ariadne import QueryType, MutationType, convert_kwargs_to_snake_case, \
     make_executable_schema, snake_case_fallback_resolvers, graphql_sync
@@ -7,30 +8,15 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-TODOS = [
-    {
-        "id": "2882u32joijd292",
-        "description": "Do some gardening",
-        "completed": False,
-        "due_date": 1601845200.0
-    },
-    {
-        "id": "6318132joijd292",
-        "description": "Shopping",
-        "completed": False,
-        "due_date": 1601758800.0
-    },
-    {
-        "id": "7168127616joijd292",
-        "description": "File taxes",
-        "completed": False,
-        "due_date": 1601758800.0
-    }
-]
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.getcwd()}/app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 type_defs = """
   type Todo {
@@ -66,56 +52,76 @@ query = QueryType()
 mutation = MutationType()
 
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
+    completed = db.Column(db.Boolean, default=False)
+    due_date = db.Column(db.Date)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "completed": self.completed,
+            "description": self.description,
+            "due_date": str(self.due_date)
+        }
+
+
 @convert_kwargs_to_snake_case
 @query.field("todos")
 def resolve_todos(obj, info):
-    return TODOS
+    todos = [todo.to_dict() for todo in Todo.query.all()]
+    return todos
 
 
 @mutation.field("createTodo")
 @convert_kwargs_to_snake_case
-def resolve_create_todo(obj, info, description, due_date, completed):
-    todo_input = {
-        "description": description,
-        "due_date": due_date,
-        "completed": completed,
-        "id": str(uuid.uuid4())
-    }
-    TODOS.append(todo_input)
-    return todo_input
+def resolve_create_todo(obj, info, description, due_date):
+    due_date = datetime.strptime(due_date, '%d-%m-%Y').date()
+    todo = Todo(
+        description=description, due_date=due_date
+    )
+    db.session.add(todo)
+    db.session.commit()
+    return todo.to_dict()
 
 
 @mutation.field("deleteTodo")
 @convert_kwargs_to_snake_case
 def resolve_delete_todo(obj, info, todo_id):
-    new_todos = list(
-        filter(lambda todo: todo if todo["id"] != todo_id else None, TODOS))
-    return {"success": True}
+    todo = Todo.query.get(todo_id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+        return {"success": True}
+    else:
+        return {"success": False}
 
 
 @mutation.field("markDone")
 @convert_kwargs_to_snake_case
 def resolve_mark_done(obj, info, todo_id):
-    new_todo = list(
-        filter(lambda todo: todo if todo["id"] == todo_id else None, TODOS))[0]
-    new_todo["completed"] = True
-    return new_todo
+    todo = Todo.query.get(todo_id)
+    todo.completed = True
+    db.session.add(todo)
+    db.session.commit()
+    return todo.to_dict()
 
 
 @mutation.field("updateDueDate")
 @convert_kwargs_to_snake_case
 def resolve_update_due_date(obj, info, todo_id, new_date):
-    new_todo = list(
-        filter(lambda todo: todo if todo["id"] == todo_id else None, TODOS))[0]
-    new_todo["due_date"] = new_date
-    return new_todo
+    todo = Todo.query.get(todo_id)
+    if todo:
+        todo.due_date = datetime.strptime(new_date, '%d-%m-%Y').date()
+    db.session.add(todo)
+    db.session.commit()
+    return todo.to_dict()
 
 
 schema = make_executable_schema(
     type_defs, [query, mutation], snake_case_fallback_resolvers
 )
-
-app = Flask(__name__)
 
 
 @app.route("/graphql", methods=["GET"])
